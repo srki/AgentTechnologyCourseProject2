280be51eac2488fca4edd16cb8febf2fa0ed9f58
Srđan Milaković <srki@outlook.com>
2016-05-07 23:46:27 +0200

    Added empty project and .gitignore.

---
+++ src/main/java/com/ftn/informatika/agents/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.ftn.informatika.agents;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author - Srđan Milaković
+ */
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+
+}
+
