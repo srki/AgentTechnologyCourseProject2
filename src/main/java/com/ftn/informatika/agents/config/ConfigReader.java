0a8c3dfd85a236f593a18260e7eb190af9900fd1
Srđan Milaković <srki@outlook.com>
2016-05-13 09:36:12 +0200

    Config reader reads from /resources folder.

--- src/main/java/com/ftn/informatika/agents/config/ConfigReader.java
+++ src/main/java/com/ftn/informatika/agents/config/ConfigReader.java
@@ -1,35 +1,31 @@
 package com.ftn.informatika.agents.config;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * @author Dragan Vidakovic
  */
 public class ConfigReader {
 
-    public List<String> getAgentsList()
-    {
-        String filePath = "src/main/java/com/ftn/informatika/agents/config/agents.txt";
+    private static final String FILE_NAME = "agents.txt";
 
-        List<String> retVal = new ArrayList<String>();
-        Scanner input = null;
-        try
-        {
-            input = new Scanner(new File(filePath));
-            String agents[] = input.nextLine().split(",");
-            for (int i=0; i<agents.length; i++)
-            {
-                retVal.add(agents[i].trim());
+    public static List<String> getAgentsList()
+    {
+        URL url = ConfigReader.class.getResource("/" + FILE_NAME);
+
+        List<String> retVal = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Collections.addAll(retVal, line.split(","));
             }
-        }
-        catch (Exception e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
